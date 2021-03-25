76ee4745dda6eacc4d1075cd6908de6cce2fab8c
N8thanaelR <nathanael.robinson@seattlecolleges.edu>
2021-03-25 09:51:34 -0700

    builds out play

---
+++ js/play.js
@@ -0,0 +1,24 @@
+//changed to var to make spans be globally available
+var spans = document.querySelectorAll("#clickSpans span");
+
+for(const mySpan of spans){
+    mySpan.addEventListener("click",function(event){
+
+       //alert(mySpan.dataset.actor);
+       highlightActor(mySpan.dataset.actor);
+    });
+
+}
+
+        //console.log(spans);
+function highlightActor(myActor){
+    for(const mySpan of spans){
+        if(mySpan.dataset.actor == myActor){//current actor, highlight
+            mySpan.style.backgroundColor = 'yellow';
+            mySpan.style.fontWeight = 'bold';
+        }else{//not current actor, no highlight 
+            mySpan.style.backgroundColor = 'white';
+            mySpan.style.fontWeight = 'normal';
+        } 
+    }
+}
